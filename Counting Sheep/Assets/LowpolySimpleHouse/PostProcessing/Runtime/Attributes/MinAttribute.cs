aa5de1030eebc67ddf45d73d31261b82484c21f6
Jinsab <kimjinou116@naver.com>
2019-10-15 08:39:05 +0900

    [Create] BackGround&Ground

---
+++ Counting Sheep/Assets/LowpolySimpleHouse/PostProcessing/Runtime/Attributes/MinAttribute.cs
@@ -0,0 +1,16 @@
+using System;
+
+namespace UnityEngine.PostProcessing
+{
+    public sealed class MinAttribute : PropertyAttribute
+    {
+        public readonly float min;
+
+        public MinAttribute(float min)
+        {
+            this.min = min;
+        }
+
+        //public static explicit operator MinAttribute(PropertyAttribute v) => throw new NotImplementedException();
+    }
+}
---
+++ Counting Sheep/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float speed = 10.0f;
+    public Rigidbody rigidbody;
+
+    private void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+        gameObject.transform.position += movement * Time.deltaTime * speed;
+    }
+}
