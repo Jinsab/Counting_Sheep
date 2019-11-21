21e119f9e7c53328b40b5ce97cda0704cd9fbb2c
Jinsab <kimjinou116@naver.com>
2019-11-21 19:37:46 +0900

    [Update] Resource Script Foundation

--- Counting Sheep/Assets/Scripts/Resource.cs
+++ Counting Sheep/Assets/Scripts/Resource.cs
@@ -4,6 +4,10 @@ using UnityEngine;
 
 public class Resource : MonoBehaviour
 {
+    public int totalResources = 100;      // 총 자원량
+    public int gatheringResources = 5;  // 자원 캐는 량
+    public float rate = 1;                // 자원 캐는 속도
+
     // Start is called before the first frame update
     void Start()
     {
