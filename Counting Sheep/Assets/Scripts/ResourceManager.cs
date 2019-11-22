03958f876b814500b16fb2f353ad3a3f6e0c2b5d
Jinsab <kimjinou116@naver.com>
2019-11-22 20:24:40 +0900

    [Create] ResourceManager

---
+++ Counting Sheep/Assets/Scripts/ResourceManager.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ResourceManager : MonoBehaviour
+{
+    public int Resource;
+    public int Energy;
+
+    private void Awake()
+    {
+        Resource = 0;
+        Energy = 0;
+    }
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
