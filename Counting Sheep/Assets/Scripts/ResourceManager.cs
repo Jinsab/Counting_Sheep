78caba2535b63bec45b3842c20fe5e2247326dda
Jinsab <kimjinou116@naver.com>
2019-11-25 09:14:04 +0900

    [Update] ResourceManager

--- Counting Sheep/Assets/Scripts/ResourceManager.cs
+++ Counting Sheep/Assets/Scripts/ResourceManager.cs
@@ -4,22 +4,38 @@ using UnityEngine;
 
 public class ResourceManager : MonoBehaviour
 {
-    public int Resource;
-    public int Energy;
+    private int m_Resource;
+    private int m_Energy;
+
+    public int Resource
+    {
+        get
+        {
+            return m_Resource;
+        }
+
+        set
+        {
+            m_Resource = Resource;
+        }
+    }
+
+    public int Energy
+    {
+        get
+        {
+            return m_Energy;
+        }
+
+        set
+        {
+            m_Energy = Energy;
+        }
+    }
 
     private void Awake()
     {
         Resource = 0;
         Energy = 0;
-    }
-
-    void Start()
-    {
-        
-    }
-
-    void Update()
-    {
-        
     }
 }
