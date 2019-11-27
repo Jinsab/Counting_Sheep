e7f1b90c79ef76514eb90204f3086e48d55aeaa2
Jinsab <kimjinou116@naver.com>
2019-11-27 17:01:06 +0900

    [Create] Navigation Baker

---
+++ Counting Sheep/Assets/Scripts/NavigationBaker.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class NavigationBaker : MonoBehaviour
+{
+    //[SerializeField] private NavMeshSurface[] surfaces;
+    //public NavMeshsS
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
