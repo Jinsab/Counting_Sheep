95c8bf4b148f7b51150249dd40ed8f02313ae99f
Jinsab <kimjinou116@naver.com>
2019-12-13 11:41:53 +0900

    [Update] OpenDoor C# Script

---
+++ Counting Sheep/Assets/Scripts/OpenDoor.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OpenDoor : MonoBehaviour
+{
+	public GameObject Door1;
+	public GameObject Door2;
+	public ResourceManager resource;
+	[SerializeField] private int OpenPoint = 0;
+	private int i = 1;
+
+    private void Update()
+    {
+		if (OpenPoint < resource.Resource)
+		{
+			if (Door1.transform.rotation.eulerAngles.y > -120f && Door2.transform.rotation.eulerAngles.y < 120f)
+			{
+				Door1.transform.rotation = Quaternion.Euler(new Vector3(0f, -i, 0f));
+				Door2.transform.rotation = Quaternion.Euler(new Vector3(0f, i, 0f));			
+			}
+			else
+			{
+				gameObject.SetActive(false);
+			}
+
+			i++;
+		}
+    }
+}
